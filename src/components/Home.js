f931ee9582a32cc6eac9f75193aa8026e24e086f
Orandifelix <125347734+Orandifelix@users.noreply.github.com>
2024-01-07 04:42:22 +0300

    intro and  bio

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,19 +3,28 @@ import React from "react";
 function Home() {
   return (
     <div className="py-12 w-4/5 m-auto text-white  min-h-screen">
-      <h2
-        className="text-5xl mb-6 font-extrabold w-3/4"
-        style={{
-          background: "linear-gradient(45deg, #8134AF, #ED3833)",
-          WebkitBackgroundClip: "text",
-          WebkitTextFillColor: "transparent",
-        }}
-      >
-        Code is elegant, design is true... welcome to my world, where creativity
-        brews.
+      <h2 className="text-6xl mb-6 font-extrabold w-4/5">
+        <span
+          style={{
+            background: "linear-gradient(45deg, #8134AF, #ED3833)",
+            WebkitBackgroundClip: "text",
+            WebkitTextFillColor: "transparent",
+          }}
+        >
+          Code is elegant, design is true...
+        </span>{" "}
+        <span style={{ color: "#FFF", marginLeft: "10px" }}>
+          welcome to my world, where creativity brews.
+        </span>
       </h2>
 
-      <h1 className="text-2xl font-bold mt-6 mb-6 ">What We Want</h1>
+      <p className="text-xl mt-6 mb-6 w-4/5 font-light ">
+        I'm Orandi Felix, a versatile Full Stack Developer and Health/Corporate
+        Communication Strategist. My focus lies in researching User/Customer
+        success, delving into the nexus of technology and communication. Through
+        code and compelling content, I innovate and strive for impactful
+        solutions, shaping a unique narrative in both domains.
+      </p>
       <p className="  border-l-2 border-white pl-4 mb-6 ">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
