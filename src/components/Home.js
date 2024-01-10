9b2c84fd7bd1a1fce30c348b643042ba52d2cf8b
Orandifelix <125347734+Orandifelix@users.noreply.github.com>
2024-01-10 19:57:07 +0300

    styles

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import { BsTwitterX } from "react-icons/bs";
 import { CiLinkedin } from "react-icons/ci";
 import { VscGithub } from "react-icons/vsc";
 import { TfiYoutube } from "react-icons/tfi";
-import { FaCode } from "react-icons/fa";
+import Skills from "./Skills";
 
 function Home() {
   return (
@@ -185,149 +185,7 @@ function Home() {
         </div>
       </div>
 
-      <p className="mb-6 text-2xl font-bold mt-6">Skills and experience</p>
-      <div className="grid grid-cols-2 gap-10">
-        <div className="flex flex-col items-center justify-center border-4 border-orange-200 p-6 rounded-lg mb-6">
-          <FaCode style={{ fontSize: "48px", alignSelf: "center" }} />
-          <h3 className="text-xl">Frontend developer</h3>
-          <ul className="list-disc pl-6">
-            <li className="flex items-center">
-              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
-                <span className="text-black">&#10003;</span>
-              </div>
-              2+ years of experience with Javascript frameworks. I can and have
-              used React, Vue, Angular, and Svelte to build Single Page
-              Applications.
-            </li>
-            <li className="flex items-center">
-              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
-                <span className="text-black">&#10003;</span>
-              </div>
-              Experience with meta-frameworks such as Next.js, Nuxt.js, Remix,
-              and Redwood.js to build SSR (Server-Side Rendering) apps.
-            </li>
-            <li className="flex items-center">
-              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
-                <span className="text-black">&#10003;</span>
-              </div>
-              Used platforms such as Vercel, Netlify, and Github Pages to deploy
-              websites and web apps.
-            </li>
-            <li className="flex items-center">
-              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
-                <span className="text-black">&#10003;</span>
-              </div>
-              Used Gatsby.js, Astro.js, and other Static Site generators to
-              build and deploy static sites, such as this blog.
-            </li>
-          </ul>
-        </div>
-        <div className="flex flex-col items-center justify-center border-4 border-orange-200 p-6 rounded-lg mb-6 ">
-          <FaCode style={{ fontSize: "48px", alignSelf: "center" }} />
-          <h3 className="text-xl">Frontend developer</h3>
-          <ul className="list-disc pl-6">
-            <li className="flex items-center">
-              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
-                <span className="text-black">&#10003;</span>
-              </div>
-              2+ years of experience with Javascript frameworks. I can and have
-              used React, Vue, Angular, and Svelte to build Single Page
-              Applications.
-            </li>
-            <li className="flex items-center">
-              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
-                <span className="text-black">&#10003;</span>
-              </div>
-              Experience with meta-frameworks such as Next.js, Nuxt.js, Remix,
-              and Redwood.js to build SSR (Server-Side Rendering) apps.
-            </li>
-            <li className="flex items-center">
-              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
-                <span className="text-black">&#10003;</span>
-              </div>
-              Used platforms such as Vercel, Netlify, and Github Pages to deploy
-              websites and web apps.
-            </li>
-            <li className="flex items-center">
-              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
-                <span className="text-black">&#10003;</span>
-              </div>
-              Used Gatsby.js, Astro.js, and other Static Site generators to
-              build and deploy static sites, such as this blog.
-            </li>
-          </ul>
-        </div>
-        <div className="flex flex-col items-center justify-center border-4 border-orange-200 p-6 rounded-lg mb-6">
-          <FaCode style={{ fontSize: "48px", alignSelf: "center" }} />
-          <h3 className="text-xl">Frontend developer</h3>
-          <ul className="list-disc pl-6">
-            <li className="flex items-center">
-              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
-                <span className="text-black">&#10003;</span>
-              </div>
-              2+ years of experience with Javascript frameworks. I can and have
-              used React, Vue, Angular, and Svelte to build Single Page
-              Applications.
-            </li>
-            <li className="flex items-center">
-              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
-                <span className="text-black">&#10003;</span>
-              </div>
-              Experience with meta-frameworks such as Next.js, Nuxt.js, Remix,
-              and Redwood.js to build SSR (Server-Side Rendering) apps.
-            </li>
-            <li className="flex items-center">
-              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
-                <span className="text-black">&#10003;</span>
-              </div>
-              Used platforms such as Vercel, Netlify, and Github Pages to deploy
-              websites and web apps.
-            </li>
-            <li className="flex items-center">
-              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
-                <span className="text-black">&#10003;</span>
-              </div>
-              Used Gatsby.js, Astro.js, and other Static Site generators to
-              build and deploy static sites, such as this blog.
-            </li>
-          </ul>
-        </div>
-        <div className="flex flex-col items-center justify-center border-2 border-orange-200 p-6 rounded-lg mb-6 ">
-          <FaCode style={{ fontSize: "48px", alignSelf: "center" }} />
-          <h3 className="text-xl">Frontend developer</h3>
-          <ul className="list-disc pl-6">
-            <li className="flex items-center">
-              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
-                <span className="text-black">&#10003;</span>
-              </div>
-              2+ years of experience with Javascript frameworks. I can and have
-              used React, Vue, Angular, and Svelte to build Single Page
-              Applications.
-            </li>
-            <li className="flex items-center">
-              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
-                <span className="text-black">&#10003;</span>
-              </div>
-              Experience with meta-frameworks such as Next.js, Nuxt.js, Remix,
-              and Redwood.js to build SSR (Server-Side Rendering) apps.
-            </li>
-            <li className="flex items-center">
-              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
-                <span className="text-black">&#10003;</span>
-              </div>
-              Used platforms such as Vercel, Netlify, and Github Pages to deploy
-              websites and web apps.
-            </li>
-            <li className="flex items-center">
-              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
-                <span className="text-black">&#10003;</span>
-              </div>
-              Used Gatsby.js, Astro.js, and other Static Site generators to
-              build and deploy static sites, such as this blog.
-            </li>
-          </ul>
-        </div>
-      </div>
+      <Skills />
     </div>
   );
 }
---
+++ src/components/Skills.js
@@ -0,0 +1,168 @@
+import React from 'react'
+import { FaCode } from "react-icons/fa";
+import { FaHardDrive } from "react-icons/fa6";
+
+
+function Skills() {
+  return (
+    <div>
+      <p className="mb-6 text-2xl font-bold mt-6">Skills and experience</p>
+      <div className="grid grid-cols-2 gap-10">
+        <div
+          style={{ border: "1px solid grey" }}
+          className="flex flex-col items-center justify-center  p-6 rounded-lg mb-6"
+        >
+          <FaCode style={{ fontSize: "48px", alignSelf: "center" }} />
+          <h3 className="text-xl">Frontend developer</h3>
+          <ul className="list-disc pl-6">
+            <li className="flex items-center">
+              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
+                <span className="text-black">&#10003;</span>
+              </div>
+              2+ years of experience with Javascript frameworks. I can and have
+              used React, Vue, Angular, and Svelte to build Single Page
+              Applications.
+            </li>
+            <li className="flex items-center">
+              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
+                <span className="text-black">&#10003;</span>
+              </div>
+              Experience with meta-frameworks such as Next.js, Nuxt.js, Remix,
+              and Redwood.js to build SSR (Server-Side Rendering) apps.
+            </li>
+            <li className="flex items-center">
+              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
+                <span className="text-black">&#10003;</span>
+              </div>
+              Used platforms such as Vercel, Netlify, and Github Pages to deploy
+              websites and web apps.
+            </li>
+            <li className="flex items-center">
+              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
+                <span className="text-black">&#10003;</span>
+              </div>
+              Used Gatsby.js, Astro.js, and other Static Site generators to
+              build and deploy static sites, such as this blog.
+            </li>
+          </ul>
+        </div>
+        <div
+          style={{ border: "1px solid grey" }}
+          className="flex flex-col items-center justify-center   p-6 rounded-lg mb-6 "
+        >
+          <FaHardDrive
+            style={{ fontSize: "48px", alignSelf: "center", padding: "10px" }}
+          />
+          <h3 className="text-xl">Backend developer</h3>
+          <ul className="list-disc pl-6">
+            <li className="flex items-center">
+              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
+                <span className="text-black">&#10003;</span>
+              </div>
+              Created and deployed backend apps running in Node.js, Python and
+              Golang
+            </li>
+            <li className="flex items-center">
+              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
+                <span className="text-black">&#10003;</span>
+              </div>
+              Deployed backend apps on Heroku, Fly.io, Cyclic and GCP
+            </li>
+            <li className="flex items-center">
+              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
+                <span className="text-black">&#10003;</span>
+              </div>
+              Experience with Ruby on rails, Laravel, Spring boot and other
+              legacy backend frameworks
+            </li>
+            <li className="flex items-center">
+              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
+                <span className="text-black">&#10003;</span>
+              </div>
+              Able to develop, test and deploy APIs/Backends using REST, GraphQL
+              and gRPC
+            </li>
+          </ul>
+        </div>
+        <div
+          style={{ border: "1px solid grey" }}
+          className="flex flex-col items-center justify-center   p-6 rounded-lg mb-6"
+        >
+          <FaCode style={{ fontSize: "48px", alignSelf: "center" }} />
+          <h3 className="text-xl">Frontend developer</h3>
+          <ul className="list-disc pl-6">
+            <li className="flex items-center">
+              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
+                <span className="text-black">&#10003;</span>
+              </div>
+              2+ years of experience with Javascript frameworks. I can and have
+              used React, Vue, Angular, and Svelte to build Single Page
+              Applications.
+            </li>
+            <li className="flex items-center">
+              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
+                <span className="text-black">&#10003;</span>
+              </div>
+              Experience with meta-frameworks such as Next.js, Nuxt.js, Remix,
+              and Redwood.js to build SSR (Server-Side Rendering) apps.
+            </li>
+            <li className="flex items-center">
+              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
+                <span className="text-black">&#10003;</span>
+              </div>
+              Used platforms such as Vercel, Netlify, and Github Pages to deploy
+              websites and web apps.
+            </li>
+            <li className="flex items-center">
+              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
+                <span className="text-black">&#10003;</span>
+              </div>
+              Used Gatsby.js, Astro.js, and other Static Site generators to
+              build and deploy static sites, such as this blog.
+            </li>
+          </ul>
+        </div>
+        <div
+          style={{ border: "1px solid grey" }}
+          className="flex flex-col items-center justify-center   p-6 rounded-lg mb-6 "
+        >
+          <FaCode style={{ fontSize: "48px", alignSelf: "center" }} />
+          <h3 className="text-xl">Frontend developer</h3>
+          <ul className="list-disc pl-6">
+            <li className="flex items-center">
+              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
+                <span className="text-black">&#10003;</span>
+              </div>
+              2+ years of experience with Javascript frameworks. I can and have
+              used React, Vue, Angular, and Svelte to build Single Page
+              Applications.
+            </li>
+            <li className="flex items-center">
+              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
+                <span className="text-black">&#10003;</span>
+              </div>
+              Experience with meta-frameworks such as Next.js, Nuxt.js, Remix,
+              and Redwood.js to build SSR (Server-Side Rendering) apps.
+            </li>
+            <li className="flex items-center">
+              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
+                <span className="text-black">&#10003;</span>
+              </div>
+              Used platforms such as Vercel, Netlify, and Github Pages to deploy
+              websites and web apps.
+            </li>
+            <li className="flex items-center">
+              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
+                <span className="text-black">&#10003;</span>
+              </div>
+              Used Gatsby.js, Astro.js, and other Static Site generators to
+              build and deploy static sites, such as this blog.
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Skills
