ed04bdeabbe5e0dcf60cea355399acb391a5952d
Orandifelix <125347734+Orandifelix@users.noreply.github.com>
2024-01-11 19:18:28 +0300

    done

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -15,34 +15,27 @@ function Skills() {
           <FaCode style={{ fontSize: "48px", alignSelf: "center" }} />
           <h3 className="text-xl">Frontend developer</h3>
           <ul className="list-disc pl-6">
-            <li className="flex items-center">
-              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
-                <span className="text-black">&#10003;</span>
-              </div>
-              2+ years of experience with Javascript frameworks. I can and have
-              used React, Vue, Angular, and Svelte to build Single Page
-              Applications.
+            <li>
+              Building dynamic user interfaces with React using components,
+              state management with hooks, and efficient navigation with React
+              Router.
             </li>
-            <li className="flex items-center">
-              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
-                <span className="text-black">&#10003;</span>
-              </div>
-              Experience with meta-frameworks such as Next.js, Nuxt.js, Remix,
-              and Redwood.js to build SSR (Server-Side Rendering) apps.
+            <li>
+              Implementing a global state management system with Redux to ensure
+              seamless data flow across complex React applications.
             </li>
-            <li className="flex items-center">
-              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
-                <span className="text-black">&#10003;</span>
-              </div>
-              Used platforms such as Vercel, Netlify, and Github Pages to deploy
-              websites and web apps.
+            <li>
+              Enhancing code reliability and development efficiency by
+              incorporating TypeScript's strong typing into React projects.
             </li>
-            <li className="flex items-center">
-              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
-                <span className="text-black">&#10003;</span>
-              </div>
-              Used Gatsby.js, Astro.js, and other Static Site generators to
-              build and deploy static sites, such as this blog.
+            <li>
+              Leveraging the Next.js framework for React to achieve server-side
+              rendering, static site generation, and efficient data fetching.
+            </li>
+            <li>
+              Crafting visually appealing and responsive user interfaces by
+              applying styling techniques, including CSS-in-JS libraries and
+              utility-first CSS frameworks.
             </li>
           </ul>
         </div>
@@ -51,36 +44,34 @@ function Skills() {
           className="flex flex-col items-center justify-center   p-6 rounded-lg mb-6 "
         >
           <FaHardDrive
-            style={{ fontSize: "48px", alignSelf: "center", padding: "10px" }}
+            style={{ fontSize: "38px", alignSelf: "center"}}
           />
           <h3 className="text-xl">Backend developer</h3>
           <ul className="list-disc pl-6">
-            <li className="flex items-center">
-              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
-                <span className="text-black">&#10003;</span>
-              </div>
-              Created and deployed backend apps running in Node.js, Python and
-              Golang
+            <li>
+              Building robust web applications using Ruby, a dynamic and
+              object-oriented programming language.
             </li>
-            <li className="flex items-center">
-              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
-                <span className="text-black">&#10003;</span>
-              </div>
-              Deployed backend apps on Heroku, Fly.io, Cyclic and GCP
+            <li>
+              Creating lightweight web applications with Sinatra, a minimalistic
+              Ruby web framework, for rapid development.
             </li>
-            <li className="flex items-center">
-              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
-                <span className="text-black">&#10003;</span>
-              </div>
-              Experience with Ruby on rails, Laravel, Spring boot and other
-              legacy backend frameworks
+            <li>
+              Developing feature-rich and scalable web applications with Ruby on
+              Rails, a full-stack web application framework.
             </li>
-            <li className="flex items-center">
-              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
-                <span className="text-black">&#10003;</span>
-              </div>
-              Able to develop, test and deploy APIs/Backends using REST, GraphQL
-              and gRPC
+            <li>
+              Integrating Ruby on Rails with React for full-stack applications,
+              ensuring seamless communication between the frontend and backend.
+            </li>
+            <li>
+              Implementing full-stack solutions by integrating Ruby-Sinatra with
+              React, combining the simplicity of Sinatra with the flexibility of
+              React.
+            </li>
+            <li>
+              Utilizing PostgreSQL, MySQL3, and SQL for efficient database
+              management and data storage in web applications.
             </li>
           </ul>
         </div>
@@ -89,36 +80,29 @@ function Skills() {
           className="flex flex-col items-center justify-center   p-6 rounded-lg mb-6"
         >
           <FaCode style={{ fontSize: "48px", alignSelf: "center" }} />
-          <h3 className="text-xl">Frontend developer</h3>
+          <h3 className="text-xl"> App Developer </h3>
           <ul className="list-disc pl-6">
-            <li className="flex items-center">
-              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
-                <span className="text-black">&#10003;</span>
-              </div>
-              2+ years of experience with Javascript frameworks. I can and have
-              used React, Vue, Angular, and Svelte to build Single Page
-              Applications.
+            <li>
+              Expertise in React Native for cross-platform mobile app
+              development, enabling efficient code sharing between iOS and
+              Android platforms.
             </li>
-            <li className="flex items-center">
-              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
-                <span className="text-black">&#10003;</span>
-              </div>
-              Experience with meta-frameworks such as Next.js, Nuxt.js, Remix,
-              and Redwood.js to build SSR (Server-Side Rendering) apps.
+            <li>
+              Proficient in utilizing Expo to streamline the development
+              process, providing tools and services for faster and easier app
+              creation.
             </li>
-            <li className="flex items-center">
-              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
-                <span className="text-black">&#10003;</span>
-              </div>
-              Used platforms such as Vercel, Netlify, and Github Pages to deploy
-              websites and web apps.
+            <li>
+              Experience in creating wireframes and prototypes using tools like
+              Figma to plan and visualize app interfaces before development.
             </li>
-            <li className="flex items-center">
-              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
-                <span className="text-black">&#10003;</span>
-              </div>
-              Used Gatsby.js, Astro.js, and other Static Site generators to
-              build and deploy static sites, such as this blog.
+            <li>
+              Knowledge of integrating additional libraries and packages with
+              React Native to enhance app functionality and features.
+            </li>
+            <li>
+              Understanding of mobile app optimization techniques to ensure
+              smooth performance and responsiveness.
             </li>
           </ul>
         </div>
@@ -127,36 +111,31 @@ function Skills() {
           className="flex flex-col items-center justify-center   p-6 rounded-lg mb-6 "
         >
           <FaCode style={{ fontSize: "48px", alignSelf: "center" }} />
-          <h3 className="text-xl">Frontend developer</h3>
+          <h3 className="text-xl"> Innovations & Explorations </h3>
           <ul className="list-disc pl-6">
-            <li className="flex items-center">
-              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
-                <span className="text-black">&#10003;</span>
-              </div>
-              2+ years of experience with Javascript frameworks. I can and have
-              used React, Vue, Angular, and Svelte to build Single Page
-              Applications.
+            <li>
+              Researching on user communication and tech success to unravel key
+              points for enhancing user experiences and product success.
             </li>
-            <li className="flex items-center">
-              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
-                <span className="text-black">&#10003;</span>
-              </div>
-              Experience with meta-frameworks such as Next.js, Nuxt.js, Remix,
-              and Redwood.js to build SSR (Server-Side Rendering) apps.
+            <li>
+              Continuously building and deploying full-stack applications with
+              seamless integration across diverse platforms like maps, payments,
+              and more. Leveraging both frontend and backend technologies for
+              robust and efficient solutions.
             </li>
-            <li className="flex items-center">
-              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
-                <span className="text-black">&#10003;</span>
-              </div>
-              Used platforms such as Vercel, Netlify, and Github Pages to deploy
-              websites and web apps.
+            <li>
+              Engaged in continuous personal studies on cutting-edge cloud
+              technologies to stay ahead of trends and advancements in cloud
+              computing.
             </li>
-            <li className="flex items-center">
-              <div className="bg-green-500 w-4 h-4 rounded-full mr-2 flex items-center justify-center">
-                <span className="text-black">&#10003;</span>
-              </div>
-              Used Gatsby.js, Astro.js, and other Static Site generators to
-              build and deploy static sites, such as this blog.
+            <li>
+              Delving into the realms of AI and machine learning to explore
+              their applications and potential impact on future technologies.
+            </li>
+            <li>
+              Ongoing studies and exploration in UI and UX, aiming to refine
+              design skills and create captivating user interfaces that elevate
+              digital experiences.
             </li>
           </ul>
         </div>
