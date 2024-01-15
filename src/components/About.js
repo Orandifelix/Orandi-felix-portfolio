cc1abfb42eaeb5080ae1bf27762d52f2175c7fa8
Orandifelix <125347734+Orandifelix@users.noreply.github.com>
2024-01-15 12:43:48 +0300

    about contact done

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from "react";
+import { FaGithub, FaLinkedin, FaYoutube, FaTwitch } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { MdAttachEmail } from "react-icons/md";
 
 function About() {
   const headings = [
@@ -30,11 +33,11 @@ function About() {
   }, [headings.length]);
 
   return (
-    <div className="bg-gray-800 w-4/5 m-auto grid grid-cols-2 gap-6 text-white py-10 h-screen">
+    <div className="bg-gray-800 w-4/5 m-auto grid grid-cols-2 gap-6 text-white py-10 ">
       <div>
-        <h1 className="  text-4xl font-extrabold">Hello, I'm Orandi Felix.</h1>
+        <h1 className="text-4xl font-extrabold">Hello, I'm Orandi Felix.</h1>
         <h2
-          className=" text-4xl font-extrabold"
+          className="text-4xl font-extrabold"
           style={{
             backgroundImage: gradients[visibleIndex],
             WebkitBackgroundClip: "text",
@@ -56,13 +59,63 @@ function About() {
             borderRadius: "10px",
           }}
         />
-        <div>
-          <h2>Follow On X</h2>
-          <h2>Follow On Github</h2>
-          <h2>Follow On LinkedIn</h2>
-          <h2>Follow On Youtube</h2>
-          <h2>Follow On Twitch</h2>
-          <h2>felixorandi@gmail.com</h2>
+        <div className="mt-20 flex flex-col gap-4">
+          <a
+            className="flex text-lg font-semibold items-center hover:text-teal-600"
+            href="https://github.com/Orandifelix"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub size={20} className="mx-2" />
+            <span> Follow On Github</span>
+          </a>
+
+          <a
+            className="flex text-lg font-semibold items-center hover:text-teal-600"
+            href="https://www.linkedin.com/in/orandi-felix"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaLinkedin size={20} className="mx-2" />
+            <span> Follow On LinkedIn</span>
+          </a>
+          <a
+            className="flex text-lg font-semibold items-center hover:text-teal-600"
+            href="https://www.youtube.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaYoutube size={20} className="mx-2" />
+            <span> Follow On Youtube</span>
+          </a>
+          <a
+            className="flex text-lg font-semibold items-center hover:text-teal-600"
+            href="https://twitter.com/fel_rand"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaXTwitter size={20} className="mx-2" />
+            <span> Follow On X</span>
+          </a>
+          <a
+            className="flex text-lg font-semibold items-center hover:text-teal-600"
+            href="https://www.twitch.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaTwitch size={20} className="mx-2" />
+            <span> Follow On Twitch</span>
+          </a>
+
+          <a
+            className="flex text-lg font-semibold items-center hover:text-teal-600 my-10"
+            href="https://www.gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <MdAttachEmail size={20} className="mx-2" />
+            <span> felixorandi@gmail.com</span>
+          </a>
         </div>
       </div>
     </div>
